a267f1970d67a1f20cb809e01612118baf3bec8b
TestError0 <ashraf.inflack@gmail.com>
2023-07-18 16:26:10 +0600

    First Commit

---
+++ src/DataType.java
@@ -0,0 +1,23 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		float b = 2;
+		double c = 3;
+		char d = 'A';
+		boolean flag1 = false;
+		String f = "Hello";
+		
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(flag1);
+		System.out.println(f);
+
+	}
+
+}
---
+++ src/Operators.java
@@ -0,0 +1,92 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		
+		//Arithmetic Operators................
+		
+				// declare variables
+				
+				int a = 5;
+				int b = 2 ;
+				int c = a + b;
+				
+				 // addition operator
+				System.out.println(c);
+				
+				 // subtraction operator
+				System.out.println(a-b);
+				
+				// multiplication operator
+				System.out.println(a*b);
+				
+				// division operator
+				System.out.println(a/b);
+				
+				// modulo operator
+				System.out.println(a%b);
+				
+				
+				
+				//Assignment Operators .................
+
+				System.out.println("\n Assignment Operators......" );
+				
+				//Create Variables
+				
+				int p = 4;
+				int var;
+				
+				// assign value using
+				var = p;
+				System.out.println("var using =: " +var);
+				
+				// assign value using =+
+				var += p;
+				System.out.println("var using +=: " + var);
+				
+				// assign value using =-
+						var -= p;
+						System.out.println("var using -=: " + var);
+						
+				// assign value using =*
+						var *= p;
+						System.out.println("var using *=: " + var);
+						
+				// assign value using =/
+						var /= p;
+						System.out.println("var using /=: " + var);
+						
+				
+						
+						System.out.println("\n  ! Relational Operators !");
+				//Relational Operators .................	
+						
+						//variables create
+						int m = 5 , n = 6;
+						
+						// value of a and b
+					    System.out.println("M is " + m + " and N is " + n);
+					    
+					 // == operator
+					    System.out.println(m==n);  //false
+					    
+					 // != operator
+					    System.out.println(m != n);  // true
+
+					    // > operator
+					    System.out.println(m > n);  // false
+
+					    // < operator
+					    System.out.println(m < n);  // true
+
+					    // >= operator
+					    System.out.println(m >= n);  // false
+
+					    // <= operator
+					    System.out.println(m <= n);  // true
+
+
+	}
+
+}
