e75363bb80b0a26b0e560a73e3b80bea8d339142
TestError0 <ashraf.inflack@gmail.com>
2023-07-20 18:52:22 +0600

    20-07

--- src/FlowControl/Ifelse.java
+++ src/FlowControl/Ifelse.java
@@ -11,6 +11,38 @@ public class Ifelse {
 		else {
 			System.out.println("False");
 		}
+		
+		
+		System.out.println("\n Java if with String");
+		//Java if with String
+		
+		String Language = "Java"; int m = 0;
+		if(Language =="Java") {
+			System.out.println("Best Programming");
+		}
+		
+		
+		//Java if...else...if Statement
+		
+		int p = 0;
+		
+		if(m>1) {
+			System.out.println("This is wrong");
+		}
+		
+		else if (m<1) {
+			System.out.println("This is True");
+		}
+		
+		else if(m == 0){
+			System.out.println("This is Write also !");
+		}
+		
+		else {
+			System.out.println("Nothing");
+		}
+		
+		
 
 	}
 
